d408837dc6b7ef65970c5a62387d30ed3bfebd2b
Valde473 <gamingracer473@gmail.com>
2025-07-09 10:08:22 +0200

    Add files via upload

---
+++ presence.js
@@ -0,0 +1,10 @@
+module.exports = async function () {
+  return {
+    details: "Play Asphalt Legends: Unite",
+    state: "Sur Switch",
+    largeImageKey: "https://upload.wikimedia.org/wikipedia/fr/thumb/d/d0/Asphalt_9_Legends_Logo.png/600px-Asphalt_9_Legends_Logo.png",
+    startTimestamp: Date.now()
+  };
+};
+
+
