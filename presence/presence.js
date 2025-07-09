700660e521d091d6c7a7d3c320f10c2f554a7c39
Valde473 <gamingracer473@gmail.com>
2025-07-09 10:19:29 +0200

    Create presence.js

---
+++ presence/presence.js
@@ -0,0 +1,9 @@
+module.exports = async function () {
+  return {
+    details: "Play Asphalt Legends: Unite",
+    state: "Sur Switch",
+    largeImageKey: "asphalt9logo",  // clé d'image, pas une URL
+    largeImageText: "Asphalt 9 Legends Logo",
+    startTimestamp: Date.now()
+  };
+};
